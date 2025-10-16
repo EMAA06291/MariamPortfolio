df2ed660abfb797103c0215f9f33c195e73e1128
EMAA06291 <emyyy212@gmail.com>
2025-10-16 21:23:30 +0300

    lastUpateee

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Header from "./Components/Header";
-import AboutMe from "./Components/AboutMe";
-import Education from './Components/Education';
-import Skills from './Components/Skills';
-import Contact from './Components/Contact';
-import Portfolio from './Components/portfolio';
+import Header from "./Components/Header.jsx";
+import AboutMe from "./Components/AboutMe.jsx";
+import Education from './Components/Education.jsx';
+import Skills from './Components/Skills.jsx';
+import Contact from './Components/Contact.jsx';
+import Portfolio from './Components/portfolio.jsx';
 function App() {
   useEffect(() => {
     AOS.init({ duration: 1000, once: true });
